b9cbac02b81e468e2270c82a005323ee04bf2b4f
SpiritSK <devonsebastiankoswara@gmail.com>
2026-03-26 22:03:05 +0800

    Adjust testutil with meetings

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -9,22 +9,31 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import seedu.address.model.AddressBook;
+import seedu.address.model.meeting.Meeting;
 import seedu.address.model.person.Person;
 
 /**
  * A utility class containing a list of {@code Person} objects to be used in tests.
  */
 public class TypicalPersons {
+    private static final UUID UUID_1 = UUID.fromString("00000000-0000-0000-0000-000000000001");
+    private static final UUID UUID_2 = UUID.fromString("00000000-0000-0000-0000-000000000002");
+    private static final UUID UUID_3 = UUID.fromString("00000000-0000-0000-0000-000000000003");
+    private static final UUID UUID_4 = UUID.fromString("00000000-0000-0000-0000-000000000004");
+    private static final UUID UUID_5 = UUID.fromString("00000000-0000-0000-0000-000000000005");
+    private static final UUID UUID_6 = UUID.fromString("00000000-0000-0000-0000-000000000006");
 
     // Assign unique IDs for each person
     public static final Person ALICE = new PersonBuilder()
-            .withId(UUID.fromString("00000000-0000-0000-0000-000000000001"))
+            .withId(UUID_1)
             .withName("Alice Pauline")
             .withEmail("alice@example.com")
             .withPhone("94351253")
@@ -32,7 +41,7 @@ public class TypicalPersons {
             .build();
 
     public static final Person BENSON = new PersonBuilder()
-            .withId(UUID.fromString("00000000-0000-0000-0000-000000000002"))
+            .withId(UUID_2)
             .withName("Benson Meier")
             .withEmail("johnd@example.com")
             .withPhone("98765432")
@@ -40,14 +49,14 @@ public class TypicalPersons {
             .build();
 
     public static final Person CARL = new PersonBuilder()
-            .withId(UUID.fromString("00000000-0000-0000-0000-000000000003"))
+            .withId(UUID_3)
             .withName("Carl Kurz")
             .withPhone("95352563")
             .withEmail("heinz@example.com")
             .build();
 
     public static final Person DANIEL = new PersonBuilder()
-            .withId(UUID.fromString("00000000-0000-0000-0000-000000000004"))
+            .withId(UUID_4)
             .withName("Daniel Meier")
             .withPhone("87652533")
             .withEmail("cornelia@example.com")
@@ -56,7 +65,7 @@ public class TypicalPersons {
 
     // Manually added - Person's details found in {@code CommandTestUtil}
     public static final Person AMY = new PersonBuilder()
-            .withId(UUID.fromString("00000000-0000-0000-0000-000000000005"))
+            .withId(UUID_5)
             .withName(VALID_NAME_AMY)
             .withPhone(VALID_PHONE_AMY)
             .withEmail(VALID_EMAIL_AMY)
@@ -64,12 +73,21 @@ public class TypicalPersons {
             .build();
 
     public static final Person BOB = new PersonBuilder()
-            .withId(UUID.fromString("00000000-0000-0000-0000-000000000006"))
+            .withId(UUID_6)
             .withName(VALID_NAME_BOB)
             .withPhone(VALID_PHONE_BOB)
             .withEmail(VALID_EMAIL_BOB)
             .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
             .build();
+
+    public static final Meeting PROJECT_MEETING = new Meeting(
+            "Project Meeting",
+            LocalDate.of(2026, 6, 15),
+            Set.of(UUID_1, UUID_3, UUID_4));
+    public static final Meeting COFFEE_MEETING = new Meeting(
+            "Coffee",
+            LocalDate.of(2026, 6, 11),
+            Set.of(UUID_1));
 
     private TypicalPersons() {} // prevents instantiation
 
@@ -81,10 +99,17 @@ public class TypicalPersons {
         for (Person person : getTypicalPersons()) {
             ab.addPerson(person);
         }
+        for (Meeting meeting : getTypicalMeetings()) {
+            ab.addMeeting(meeting);
+        }
         return ab;
     }
 
     public static List<Person> getTypicalPersons() {
         return new ArrayList<>(Arrays.asList(ALICE, BENSON, CARL, DANIEL));
+    }
+
+    public static List<Meeting> getTypicalMeetings() {
+        return new ArrayList<>((Arrays.asList(PROJECT_MEETING, COFFEE_MEETING)));
     }
 }
