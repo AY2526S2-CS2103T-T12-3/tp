10da46cd7f57dbfa041502db3b980f10e2973a1b
Raylong268 <shanetanjun@gmail.com>
2026-04-11 12:59:05 +0800

    Change tags to case-insensitive

--- src/main/java/seedu/address/model/tag/Tag.java
+++ src/main/java/seedu/address/model/tag/Tag.java
@@ -24,7 +24,12 @@ public class Tag {
     public Tag(String tagName) {
         requireNonNull(tagName);
         checkArgument(isValidTagName(tagName), MESSAGE_CONSTRAINTS);
-        this.tagName = tagName;
+
+        if (tagName.equalsIgnoreCase("STAR")) {
+            this.tagName = "STAR";
+        } else {
+            this.tagName = tagName;
+        }
     }
 
     /**
@@ -48,12 +53,12 @@ public class Tag {
         }
 
         Tag otherTag = (Tag) other;
-        return tagName.equals(otherTag.tagName);
+        return tagName.equalsIgnoreCase(otherTag.tagName);
     }
 
     @Override
     public int hashCode() {
-        return tagName.hashCode();
+        return tagName.toLowerCase().hashCode();
     }
 
     /**
